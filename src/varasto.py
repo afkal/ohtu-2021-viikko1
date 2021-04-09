13f0ee774bcdaa94fc027545dd2084ff7619e095
Timo Laitila <timo.t.laitila@gmail.com>
2021-04-09 17:33:07 +0300

    Braking change for varasto.py

--- src/varasto.py
+++ src/varasto.py
@@ -1,11 +1,11 @@
 class Varasto:
-    def __init__(self, tilavuus, alku_saldo = 0):
+    de __init__(self, tilavuus, alku_saldo = 0):
         if tilavuus > 0.0:
             self.tilavuus = tilavuus
         else:
             # virheellinen, nollataan
             self.tilavuus = 0.0
-        
+
         if alku_saldo < 0.0:
             # virheellinen, nollataan
             self.saldo = 0.0
@@ -31,7 +31,7 @@ class Varasto:
     def ota_varastosta(self, maara):
         if maara < 0:
             return 0.0
-        if maara > self.saldo: 
+        if maara > self.saldo:
             kaikki_mita_voidaan = self.saldo
             self.saldo = 0.0
 
@@ -40,6 +40,6 @@ class Varasto:
         self.saldo = self.saldo - maara
 
         return maara
-    
+
     def __str__(self):
         return f"saldo = {self.saldo}, vielä tilaa {self.paljonko_mahtuu()}"
