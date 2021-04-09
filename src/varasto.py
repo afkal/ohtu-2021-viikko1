7b9d5feb5e2afce3d77429db5d88d0a7b3a2e8fa
Timo Laitila <timo.t.laitila@gmail.com>
2021-04-09 17:36:43 +0300

    Braking change fixed

--- src/varasto.py
+++ src/varasto.py
@@ -1,5 +1,5 @@
 class Varasto:
-    de __init__(self, tilavuus, alku_saldo = 0):
+    def __init__(self, tilavuus, alku_saldo = 0):
         if tilavuus > 0.0:
             self.tilavuus = tilavuus
         else:
